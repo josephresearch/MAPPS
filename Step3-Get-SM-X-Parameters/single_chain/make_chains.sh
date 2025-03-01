62bfbad71443e17f630280f8b486befa7cb434b2
ummagumma0 <auemelianova@gmail.com>
2025-03-01 16:40:59 -0500

    file upload

--- Step3-Get-SM-X-Parameters/single_chain/make_chains.sh
+++ Step3-Get-SM-X-Parameters/single_chain/make_chains.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-folders=('2-methylpropane')
+folders=('Aniline')
 
 for folder in "${folders[@]}"; do
     mkdir -p "$folder"
@@ -13,7 +13,6 @@ for folder in "${folders[@]}"; do
     for chain_ind in {2..8}; do
         for i in {1..3}; do
             mkdir -p "$folder/seq$chain_ind/seq${chain_ind}_$i"
-            cp "seq_$chain_ind/packmol.gro" "$folder/seq$chain_ind/seq${chain_ind}_$i"
             cp "seq_$chain_ind/chain_20_$chain_ind.top" "$folder/seq$chain_ind/seq${chain_ind}_$i/chain_20_$chain_ind.top"
             cp "seq_$chain_ind/chain_20_$chain_ind.top" "$folder/seq$chain_ind/seq${chain_ind}_$i/chain_20_0.top"
             cp "seq_$chain_ind/chain_20_$chain_ind.gro" "$folder/seq$chain_ind/seq${chain_ind}_$i"
@@ -21,16 +20,15 @@ for folder in "${folders[@]}"; do
             cp "$folder/topol.top" "$folder/seq$chain_ind/seq${chain_ind}_$i"
             cp "$folder/run_sim.sh" "$folder/seq$chain_ind/seq${chain_ind}_$i"
             cp "$folder/solvate.sh" "$folder/seq$chain_ind/seq${chain_ind}_$i"
-            cp "$folder/npt.mdp" "$folder/seq$chain_ind/seq${chain_ind}_$i"
             cp "$folder/nvt.mdp" "$folder/seq$chain_ind/seq${chain_ind}_$i"
             cp "$folder/em.mdp" "$folder/seq$chain_ind/seq${chain_ind}_$i"
             
-            cp "2-methylpropane/seq$chain_ind/seq${chain_ind}_1/npt.gro" "$folder/seq$chain_ind/seq${chain_ind}_$i"
+            cp "2-Methylpropane/gmx/seq$chain_ind/npt.gro" "$folder/seq$chain_ind/seq${chain_ind}_$i"
             
             cd  "$folder/seq$chain_ind/seq${chain_ind}_$i"
 
-            bash solvate.sh
-            sbatch run_sim.sh
+            # bash solvate.sh
+            # sbatch run_sim.sh
             cd ../../../
         done
     done
